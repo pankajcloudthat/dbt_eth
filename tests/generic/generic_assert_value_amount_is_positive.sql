c9b244a968d930bb28d6816a3fb0f0fc22cf9736
Pankaj Choudhary <pankajc@cloudthat.com>
2026-07-06 16:37:33 +0530

    Adding generic test

---
+++ tests/generic/generic_assert_value_amount_is_positive.sql
@@ -0,0 +1,10 @@
+{% test assert_value_amount_positive(model, column_name, field) %}
+
+select
+    {{ field }},
+    sum({{ column_name }}) as total_amount
+from {{ model }}
+group by {{ field }}
+having total_amount < 0
+
+{% endtest %}
