ce723d718da11303b8753c58574e1473b6dbde99
Pankaj Choudhary <pankajc@cloudthat.com>
2026-07-06 09:34:36 +0530

    Changes added to Slim CI pipeline

--- models/marts/analytics/eth_activity_per_day.sql
+++ models/marts/analytics/eth_activity_per_day.sql
@@ -9,7 +9,7 @@ select
     date, 
     transaction_category, 
     count(*) as tx_count, 
-    {{ conversion('value', '18')}} as sum_eth_value
+    {{ conversion('value', '18')}} as eth_sum_value
 
 from {{ ref("stg_transactions_enriched") }}
 
