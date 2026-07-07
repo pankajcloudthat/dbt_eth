677b269035066932e2508831e0b05ecf8a09e64f
Pankaj Choudhary <pankajc@cloudthat.com>
2026-07-07 12:38:11 +0530

    added microbatch

--- models/base/stg_contracts.sql
+++ models/base/stg_contracts.sql
@@ -1,10 +1,20 @@
-{{ config(materialized = 'view') }}
+{{ 
+    config(
+        materialized="incremental", 
+        incremental_strategy="microbatch", 
+        begin='2026-07-03',
+        event_time='date',
+        batch_size='day',
+		lookback=2, 
+		concurrent_batches=false
+    ) 
+}}
 
-SELECT
-    address,
-    block_number,
-    bytecode,
-    date,
+select 
+    address, 
+    block_number, 
+    bytecode, 
+    date, 
     last_modified
 
-FROM {{ source('eth', 'contracts') }}
+from {{ source("eth", "contracts") }}
